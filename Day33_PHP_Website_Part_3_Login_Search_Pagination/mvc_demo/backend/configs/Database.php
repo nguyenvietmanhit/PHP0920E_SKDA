a85b0102178dfa76541f5f45fbf53238bac0d26c
Nguyen Viet Manh <nvmanh@cmc.com.vn>
2021-01-25 09:44:05 +0700

    update

---
+++ Day33_PHP_Website_Part_3_Login_Search_Pagination/mvc_demo/backend/configs/Database.php
@@ -0,0 +1,7 @@
+<?php
+class Database {
+  const DB_DSN = 'mysql:host=localhost;dbname=php0520e_project;charset=utf8';
+  const DB_USERNAME = 'root';
+  const DB_PASSWORD = '';
+}
+//Ctrl + Shift + F -> PHPStorm
